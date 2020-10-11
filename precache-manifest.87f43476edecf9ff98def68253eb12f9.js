99c7b8d2efa009df476f85c5cf580464486f321b
Hedryan Josué Angulo Rojas <hedryan.rojas@avantica.com>
2020-10-11 00:44:46 -0600

    Updates

---
+++ precache-manifest.87f43476edecf9ff98def68253eb12f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc10648da6e601b04a93d7f478d4e4bc",
+    "url": "/spreadsheet-frontend/index.html"
+  },
+  {
+    "revision": "86ed002361261270f234",
+    "url": "/spreadsheet-frontend/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "3ba5f3d842e248750aae",
+    "url": "/spreadsheet-frontend/static/css/main.7070aeb2.chunk.css"
+  },
+  {
+    "revision": "86ed002361261270f234",
+    "url": "/spreadsheet-frontend/static/js/2.52263982.chunk.js"
+  },
+  {
+    "revision": "49fe2bc6ca6e77e8e48c05ef1eac3fbf",
+    "url": "/spreadsheet-frontend/static/js/2.52263982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ba5f3d842e248750aae",
+    "url": "/spreadsheet-frontend/static/js/main.1c3aae7a.chunk.js"
+  },
+  {
+    "revision": "f5e16fcf58a06a81d789",
+    "url": "/spreadsheet-frontend/static/js/runtime-main.c122a3d9.js"
+  },
+  {
+    "revision": "ed1080815ff773768ae97286f2fc8940",
+    "url": "/spreadsheet-frontend/static/media/logo.ed108081.png"
+  }
+]);
